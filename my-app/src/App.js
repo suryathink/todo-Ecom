f9e9e718976ff5b78a78469031afa9c8636098f2
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-07-15 19:52:08 +0530

    Code Formatted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import TodoList from './Components/Todo/TodoList';
-import NavbarComponent from './Components/Navbar';
-import { Routes, Route } from "react-router-dom"
-import Home from './Components/Home';
-import ApiIntegrationPage from './Components/Ecom/ApiIntegrationPage';
-import Cart from './Components/Ecom/Cart';
-import AboutPage from './Components/Ecom/About';
-import ContactForm from "./Components/Ecom/Contact"
+import "./App.css";
+import TodoList from "./Components/Todo/TodoList";
+import NavbarComponent from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import ApiIntegrationPage from "./Components/Ecom/ApiIntegrationPage";
+import Cart from "./Components/Ecom/Cart";
+import AboutPage from "./Components/Ecom/About";
+import ContactForm from "./Components/Ecom/Contact";
 function App() {
   return (
     <div className="App">
-    <NavbarComponent/>
+      <NavbarComponent />
 
-     <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="/todo" element={ <TodoList/> } />
-        <Route path="/products" element={ <ApiIntegrationPage/> } />
-        <Route path="/cart" element={ <Cart/> } />
-        <Route path="/about" element={ <AboutPage/> } />
-        <Route path="contact" element={ <ContactForm/> } /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<TodoList />} />
+        <Route path="/products" element={<ApiIntegrationPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactForm />} />
       </Routes>
     </div>
   );
--- my-app/src/Components/Ecom/Contact.jsx
+++ my-app/src/Components/Ecom/Contact.jsx
@@ -1,69 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log('Form submitted:', { name, email, message });
+    console.log("Form submitted:", { name, email, message });
     // Clear form fields after submission
-    setName('');
-    setEmail('');
-    setMessage('');
+    setName("");
+    setEmail("");
+    setMessage("");
   };
 
   return (
     <div>
-      <div style={{width:"45%",margin:"auto",marginTop:"15px"}}> 
-
-      <form onSubmit={handleSubmit} style={{ maxWidth: '400px' }}>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="name" style={{ display: 'block', marginBottom: '0.5rem' }}>
-            Name:
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            style={{ width: '100%', padding: '0.5rem' }}
-            required
-          />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="email" style={{ display: 'block', marginBottom: '0.5rem' }}>
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={{ width: '100%', padding: '0.5rem' }}
-            required
-          />
-        </div>
-        <div style={{ marginBottom: '1rem' }}>
-          <label htmlFor="message" style={{ display: 'block', marginBottom: '0.5rem' }}>
-            Message:
-          </label>
-          <textarea
-            id="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            style={{ width: '100%', padding: '0.5rem', minHeight: '100px' }}
-            required
-          ></textarea>
-        </div>
-        <button onClick={()=>{alert("Form Submitted")}} type="submit" style={{ padding: '0.5rem 1rem', backgroundColor: 'lightblue', border: 'none', borderRadius: '4px' }}>
-          Submit
-        </button>
-      </form>
-    </div>
+      <div style={{ width: "45%", margin: "auto", marginTop: "15px" }}>
+        <form onSubmit={handleSubmit} style={{ maxWidth: "400px" }}>
+          <div style={{ marginBottom: "1rem" }}>
+            <label
+              htmlFor="name"
+              style={{ display: "block", marginBottom: "0.5rem" }}
+            >
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              style={{ width: "100%", padding: "0.5rem" }}
+              required
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <label
+              htmlFor="email"
+              style={{ display: "block", marginBottom: "0.5rem" }}
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ width: "100%", padding: "0.5rem" }}
+              required
+            />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <label
+              htmlFor="message"
+              style={{ display: "block", marginBottom: "0.5rem" }}
+            >
+              Message:
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              style={{ width: "100%", padding: "0.5rem", minHeight: "100px" }}
+              required
+            ></textarea>
+          </div>
+          <button
+            onClick={() => {
+              alert("Form Submitted");
+            }}
+            type="submit"
+            style={{
+              padding: "0.5rem 1rem",
+              backgroundColor: "lightblue",
+              border: "none",
+              borderRadius: "4px",
+            }}
+          >
+            Submit
+          </button>
+        </form>
       </div>
+    </div>
   );
 };
 
