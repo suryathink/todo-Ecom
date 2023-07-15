3eb0bcd31aebde5af4f9d22f4ef954fc9a66bddb
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-07-15 19:21:50 +0530

    Contact Page added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import Home from './Components/Home';
 import ApiIntegrationPage from './Components/Ecom/ApiIntegrationPage';
 import Cart from './Components/Ecom/Cart';
 import AboutPage from './Components/Ecom/About';
+import ContactForm from "./Components/Ecom/Contact"
 function App() {
   return (
     <div className="App">
@@ -13,11 +14,11 @@ function App() {
 
      <Routes>
         <Route path="/" element={ <Home/> } />
-        {/* <Route path="contact" element={ <Contact/> } />  */}
         <Route path="/todo" element={ <TodoList/> } />
         <Route path="/products" element={ <ApiIntegrationPage/> } />
         <Route path="/cart" element={ <Cart/> } />
         <Route path="/about" element={ <AboutPage/> } />
+        <Route path="contact" element={ <ContactForm/> } /> 
       </Routes>
     </div>
   );
--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -16,12 +16,11 @@ function NavbarComponent() {
         <Container>
           <Navbar.Brand ><Link className='linkStyle' to ="/" >XYZ Store</Link></Navbar.Brand>
           <Nav className="me-auto">
-         {/* <Link ><Nav.Link >Home</Nav.Link></Link>     */}
-         {/* <Link ><Nav.Link >Home</Nav.Link></Link>     */}
         <Nav.Link > <Link className='linkStyle' to ="/todo" >TodoList </Link> </Nav.Link> 
         <Nav.Link > <Link className='linkStyle' to ="/products" >Products Page </Link> </Nav.Link> 
         <Nav.Link > <Link className='linkStyle' to ="/cart" >Cart Page </Link> </Nav.Link> 
         <Nav.Link > <Link className='linkStyle' to ="/about" >About Page </Link> </Nav.Link> 
+        <Nav.Link > <Link className='linkStyle' to ="/contact" >Contact Page </Link> </Nav.Link> 
           </Nav>
         </Container>
       </Navbar>
